1163d9f45c65eb10647f22cbf6ef3d181d33541f
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-01 18:10:52 +0000

    Convert to ES6 shorthands

--- src/app/component-helper.ts
+++ src/app/component-helper.ts
@@ -1,9 +1,7 @@
 declare var System: any; // SystemJS imported globally
 
 export default function LoadComponentAsync(name: string, path: string) {
-    return function() {
-        // System.import returns an object with keys = exported object (class, etc)
-        // "default" returns the default object
-        return System.import(path).then((c: any) => c[name]);
-    }
+    // System.import returns an object with keys = exported object (class, etc)
+    // "default" returns the default object
+    return () => System.import(path).then((c: any) => c[name]);
 }
