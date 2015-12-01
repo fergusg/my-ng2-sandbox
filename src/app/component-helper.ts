a47ae258a4838e8e31e08cb00a1787a6a3cb1bea
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-01 20:46:15 +0000

    Add helper function for routes

--- src/app/component-helper.ts
+++ src/app/component-helper.ts
@@ -1,6 +1,6 @@
 declare var System: any; // SystemJS imported globally
 
-export default function LoadComponentAsync(name: string, path: string) {
+export default function LoadComponentAsync(path: string, name: string  = 'default') {
     // System.import returns an object with keys = exported object (class, etc)
     // "default" returns the default object
     return () => System.import(path).then((c: any) => c[name]);
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,63 +1,46 @@
 import {bootstrap, Component, View, provide, CORE_DIRECTIVES, NgFor}
-    from 'angular2/angular2';
+from 'angular2/angular2';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, Route,
-    AsyncRoute, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+AsyncRoute, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 import LoadComponentAsync from "./component-helper";
 
 import HeroesComponent from './components/heroes/heroes-component';
 import HomeComponent from './components/home/home';
 import GreetingComponent from './components/greeting/greeting-component';
-import TreeViewDemoComponent from "./components/tree-view/tree-view-demo";
+import TreeViewComponent from "./components/tree-view/tree-view-demo";
 import AddressBookComponent from "./components/address-book/address-book";
 import SandBoxComponent from "./components/sandbox/sandbox";
 import VetoComponent from "./components/veto/veto";
 
 console.log('Loading index.js...');
 
-@Component ({
+function makeRoute(component: any, path: string = null) {
+    const name = component.name.replace(/Component$/, '');
+    return new Route(
+        {
+            path: path ? path : '/' + name.toLowerCase(),
+            component: component,
+            name: name
+        }
+    );
+}
+
+@Component({
     selector: 'index',
 })
 @RouteConfig([
-    new Route({
-        path : '/heroes',
-        component: HeroesComponent,
-        name: 'Heroes'
-    }),
-    new Route({
-        path: '/',
-        component: HomeComponent,
-        name: 'Home'
-    }),
-    new Route({
-        path: '/greeting',
-        component: GreetingComponent,
-        name: 'Greeting'
-    }),
-    new Route({
-        path: '/treeview',
-        component: TreeViewDemoComponent,
-        name: 'TreeView'
-    }),
-    new Route({
-        path: '/addressbook',
-        component: AddressBookComponent,
-        name: 'AddressBook'
-    }),
-    new Route({
-        path: '/sandbox',
-        component: SandBoxComponent,
-        name: 'SandBox'
-    }),
-    new Route({
-        path: '/veto',
-        component: VetoComponent,
-        name: 'Veto'
-    }),
+    makeRoute(HomeComponent, '/'),
+    makeRoute(HeroesComponent),
+    makeRoute(GreetingComponent),
+    makeRoute(TreeViewComponent),
+    makeRoute(AddressBookComponent),
+    makeRoute(SandBoxComponent),
+    makeRoute(VetoComponent),
     new AsyncRoute({
         path: '/lazy',
-        loader: LoadComponentAsync('LazyLoaded', './app/components/lazy-loaded/lazy-loaded'),
+        loader: LoadComponentAsync('./app/components/lazy-loaded/lazy-loaded'),
         name: 'Lazy'
     })
 ])
@@ -71,11 +54,12 @@ console.log('Loading index.js...');
     directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES]
 })
 class Index {
-    public routes:string[] = [
+    public routes: string[] = [
         'Home', 'Greeting', 'Heroes', 'TreeView',
         'AddressBook', 'Lazy', 'Veto', 'SandBox'
     ];
+
 }
 
 bootstrap(Index, [HTTP_PROVIDERS, ROUTER_PROVIDERS,
-    provide(LocationStrategy, {useClass: HashLocationStrategy})]);
+    provide(LocationStrategy, { useClass: HashLocationStrategy })]);
