089838230a8ec4a8ba4691e47f9327d29931014b
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-02 18:28:31 +0000

    Automagic the routes

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,37 +1,36 @@
-import {bootstrap, Component, View, provide, CORE_DIRECTIVES, NgFor}
-from 'angular2/angular2';
-import {HTTP_PROVIDERS} from 'angular2/http';
+import {bootstrap, Component, View, provide, CORE_DIRECTIVES, NgFor} from "angular2/angular2";
+import {HTTP_PROVIDERS} from "angular2/http";
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, Route,
-AsyncRoute, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+AsyncRoute, LocationStrategy, HashLocationStrategy} from "angular2/router";
 
 import LoadComponentAsync from "./component-helper";
 
-import HeroesComponent from './components/heroes/heroes-component';
-import HomeComponent from './components/home/home';
-import GreetingComponent from './components/greeting/greeting-component';
+import HeroesComponent from "./components/heroes/heroes-component";
+import HomeComponent from "./components/home/home";
+import GreetingComponent from "./components/greeting/greeting-component";
 import TreeViewComponent from "./components/tree-view/tree-view-demo";
 import AddressBookComponent from "./components/address-book/address-book";
-import SandBoxComponent from "./components/sandbox/sandbox";
 import VetoComponent from "./components/veto/veto";
 
-console.log('Loading index.js...');
+import SandBoxComponent from "./components/sandbox/sandbox";
 
+// Can't yet find a way to include this with the class
+const ROUTE_NAMES: string[] = [];
 function makeRoute(component: any, path: string = null) {
-    const name = component.name.replace(/Component$/, '');
-    return new Route(
-        {
-            path: path ? path : '/' + name.toLowerCase(),
-            component: component,
-            name: name
-        }
-    );
+    const name = component.name.replace(/Component$/, "");
+    path = path ? path : "/" + name.toLowerCase();
+
+    ROUTE_NAMES.push(name);
+    // ES6: key-name = value-name by default
+    // i.e., this is shorthand for {component:component, name:name, path:path}
+    return new Route({ component, name, path });
 }
 
 @Component({
-    selector: 'index',
+    selector: "index"
 })
 @RouteConfig([
-    makeRoute(HomeComponent, '/'),
+    makeRoute(HomeComponent, "/"),
     makeRoute(HeroesComponent),
     makeRoute(GreetingComponent),
     makeRoute(TreeViewComponent),
@@ -39,10 +38,10 @@ function makeRoute(component: any, path: string = null) {
     makeRoute(SandBoxComponent),
     makeRoute(VetoComponent),
     new AsyncRoute({
-        path: '/lazy',
-        loader: LoadComponentAsync('./app/components/lazy-loaded/lazy-loaded'),
-        name: 'Lazy'
-    })
+        loader: LoadComponentAsync("./app/components/lazy-loaded/lazy-loaded"),
+        name: "Lazy",
+        path: "/lazy",
+    }),
 ])
 @View({
     template: `
@@ -54,12 +53,9 @@ function makeRoute(component: any, path: string = null) {
     directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES]
 })
 class Index {
-    public routes: string[] = [
-        'Home', 'Greeting', 'Heroes', 'TreeView',
-        'AddressBook', 'Lazy', 'Veto', 'SandBox'
-    ];
-
+    public routes = ROUTE_NAMES;
 }
 
 bootstrap(Index, [HTTP_PROVIDERS, ROUTER_PROVIDERS,
-    provide(LocationStrategy, { useClass: HashLocationStrategy })]);
+    provide(LocationStrategy, { useClass: HashLocationStrategy })
+]);
