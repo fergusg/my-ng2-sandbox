565891a429d0c619895124b50107b6f14b797c91
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-09 17:12:03 +0000

    Make Home a bit more interesting

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,30 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, View, CORE_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
-    selector: 'home'
-})
-@View({
+    selector: 'home',
     template: `
-    <h1>Home Page</h1>
-    `
+    <h1>{{title}}</h1>
+    <span *ng-for="#d of data">{{d}} </span>
+    `,
+    directives: [CORE_DIRECTIVES]
 })
-export default class HomeComponent { }
+export default class HomeComponent {
+    public title = "Home Page";
+    public data: any[] = [];
+
+    constructor() {
+        this.load(200);
+    }
+
+    load(N:number = 100) {
+        for (var i = 0, delta = 2000.0/N; i < N; i++) {
+            ((i:number) => {
+                window.setTimeout(() => this.data.push(`${i}, `), i * delta);
+            })(i);
+        }
+        // tsc doesn't support let in loops properly yet.
+        // for (let q = 0, delta = 2000.0 / N; q < N; q++) {
+        //     window.setTimeout(() => this.data.push(`${q}, `), q * delta);
+        // }
+    }
+}
