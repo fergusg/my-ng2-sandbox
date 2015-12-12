c97dec016a3d777af844797925574fc7be0d8ed9
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-12 15:35:57 +0000

    Only ignore js inside app/

---
+++ src/gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var ts = require('gulp-typescript');
+var rename = require('gulp-rename');
+var merge = require('merge-stream');
+
+
+var paths = {
+    "ts" : [
+        "app/**/*.ts"
+    ],
+    "other": [
+        '**/*.*',
+        '!index.*',
+        '!**/*.ts',
+        "!node_modules/**",
+        "!jspm_packages/**"
+        ],
+    "libs": [
+        'node_modules/systemjs/**',
+        'node_modules/angular2/bundles/**',
+        'node_modules/rxjs/**',
+        'jspm_packages/**',
+        '!**/tests/**',
+        '!**/test/**',
+        '!**/docs/**',
+        '!**/typings/**',
+        '!**/*.json',
+        '!**/systemjs/lib/**',
+        '!**/*.umd.*'
+    ],
+    "config": [
+        'jspm_packages/system.js',
+        // 'config.js'
+    ],
+
+    "release": "../release"
+
+};
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var rename = require("gulp-rename");
+
+gulp.task("build-ts", function () {
+    var tsProject = ts.createProject('./tsconfig.json');
+
+    return gulp
+        .src(paths.ts, { base: "./" })
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject))
+        .pipe(rename(function (path) {
+            path.extname = ".js";
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.release));
+});
+
+gulp.task('build-other', ['build-static', 'build-libs']);
+
+gulp.task('build-static', function() {
+	var base = gulp
+       .src(paths.other)
+	   .pipe(gulp.dest(paths.release));
+
+    var index = gulp
+       .src('index.release.html')
+        .pipe(rename(function (path) {
+            path.basename = "index";
+        }))
+       .pipe(gulp.dest(paths.release));
+
+   return merge(base, index);
+});
+
+gulp.task('build-libs', function() {
+	var libs = gulp
+       .src(paths.libs, { base: "./" })
+	   .pipe(gulp.dest(paths.release));
+
+    var config = gulp
+       .src(paths.config)
+	   .pipe(gulp.dest(paths.release));
+
+    return merge(libs, config);
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.ts, ['build-ts']);
+    gulp.watch(paths.other, ['build-other']);
+});
+
+gulp.task('default',['build-ts','build-other']);
