4bf2f03218aa85b3fa659c55d6d39131d2d9f28b
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-13 20:52:11 +0000

    No need to make protected

--- src/app/components/address-book/address-book-service.ts
+++ src/app/components/address-book/address-book-service.ts
@@ -2,7 +2,7 @@ import {Http} from 'angular2/http'
 import CachingService from "../../utils/caching-service";
 
 export default class AddressBookService extends CachingService {
-    constructor(protected http: Http) {
+    constructor(http: Http) {
         super(http);
         this.src = "./people.json";
     }
--- src/app/components/heroes/heroes-service.ts
+++ src/app/components/heroes/heroes-service.ts
@@ -2,7 +2,7 @@ import {Http} from 'angular2/http'
 import CachingService from "../../utils/caching-service";
 
 export default class HeroesService extends CachingService {
-    constructor(protected http: Http) {
+    constructor(http: Http) {
         super(http);
         this.src = "./heroes.json";
     }
