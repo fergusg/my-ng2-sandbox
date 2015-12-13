ecc6206b11540e353d5075d585b3f3975ba743ff
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-13 19:12:44 +0000

    Add basic caching

--- src/app/components/address-book/address-book-service.ts
+++ src/app/components/address-book/address-book-service.ts
@@ -1,12 +1,30 @@
 import {Http, Response} from 'angular2/http'
 import {Injectable} from 'angular2/angular2'
+import {Observable} from 'rxjs/Observable'; // .map() & .of() imported in index.ts
 
 @Injectable()
 export default class AddressBookService {
-    constructor(private http: Http) {
+    private static PEOPLE: any;
+    private static SRC = "./people.json";
+
+    constructor(private http: Http) {}
+
+    private static cached() {
+        return AddressBookService.PEOPLE
+            ? Observable.of(AddressBookService.PEOPLE)
+            : null;
+    }
+
+    private static load(http: Http) {
+        return http.get(AddressBookService.SRC).map(
+            (res: Response) => {
+                return AddressBookService.PEOPLE = res.json();
+            }
+        );
     }
 
     public getEntries() {
-        return this.http.get('./people.json').map((res: Response) => res.json());
+        const ABS = AddressBookService;
+        return ABS.cached() || ABS.load(this.http);
     }
 }
