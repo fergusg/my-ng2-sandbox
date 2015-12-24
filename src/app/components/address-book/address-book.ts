4248e82d0b342606337c868e85e41086a5b45ec8
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-24 21:30:58 +0000

    Make array top level

--- src/app/components/address-book/address-book.ts
+++ src/app/components/address-book/address-book.ts
@@ -2,13 +2,18 @@ import {Component} from "angular2/core";
 
 import BookService from "./address-book-service";
 
+interface IPerson {
+    name: string;
+    address: string;
+}
+
 const __TEMPLATE: string = `
     <h1>Dependency Injection</h1>
     <h2>{{title}}</h2>
 
-    <div *ngIf="!result">loading...</div>
-    <table class="table" *ngIf="result">
-        <tr *ngFor="#person of result.people">
+    <div *ngIf="!people">loading...</div>
+    <table class="table" *ngIf="people">
+        <tr *ngFor="#person of people">
             <td class="name">{{person.name}}</td>
             <td class="address">{{person.address}}</td>
         </tr>
@@ -25,18 +30,18 @@ const __CSS: string = `
 `;
 
 @Component({
-    providers: [BookService],
     selector: "address-book",
+    providers: [BookService],
     styles: [__CSS],
     template: __TEMPLATE,
 })
 class AddressBook {
-    protected title: string= "My Address Book";
-    private result: any;
+    protected title: string = "My Address Book";
+    private people: IPerson[];
 
     constructor(bookService: BookService) {
         bookService.get().subscribe(
-            (res: any): any => this.result = res,
+            (res: any): IPerson[] => this.people = res,
             (): any => null
         );
     }
