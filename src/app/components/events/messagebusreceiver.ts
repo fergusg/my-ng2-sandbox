56e55f29f5c4709abd6c50b7e773848642e132c2
Fergus Gallagher <fergus@gooses.co.uk>
2015-12-25 20:13:33 +0000

    remove dud logging

--- src/app/components/events/messagebusreceiver.ts
+++ src/app/components/events/messagebusreceiver.ts
@@ -6,17 +6,15 @@ import MessageBus from "./messagebus";
 })
 export default class MessageBusReceiver {
     constructor(private messageBus: MessageBus, private elem: ElementRef) {
-        console.log(this.messageBus.emitter);
-        let r: any = this.messageBus
+        this.messageBus
         .emitter
         .debounceTime(100)
         .subscribe(
             (t: any) => {
                 elem.nativeElement.innerText = t;
             },
-            (err: any) => console.log("error", err),
+            (err: any) => console.error("error", err),
             () => console.log("complete")
         );
-        console.log(r);
     }
 }
