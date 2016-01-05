7925aca6fe9da16d5888ee99abfebe40c6024cc7
Fergus Gallagher <fergus@gooses.co.uk>
2016-01-05 19:19:43 +0000

    Refromat

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,21 +1,21 @@
 var path = require("path");
 var __dirname;
 module.exports = {
-  entry: {
-    app: ["./app/index.ts"]
-  },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/assets/",
-    filename: "bundle.js"
-  },
-  module: {
-      loaders: [
-          { test: /\.tsx?/, loader: "webpack-typescript?target=ES5&jsx=react" },
-          { test: /\.html/, loader: "html" },
-      ]
-  },
-  resolve: {
-      extensions: ["", ".ts", ".js"]
-  }
+    entry: {
+        app: ["./app/index.ts"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/assets/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.tsx?/, loader: "webpack-typescript?target=ES5&jsx=react" },
+            { test: /\.html/, loader: "html" },
+        ]
+    },
+    resolve: {
+        extensions: ["", ".ts", ".js"]
+    }
 };
