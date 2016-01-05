5214f4e85fa1b3f6a2f314ee0a0dbc4fa8300a1b
Fergus Gallagher <fergus@gooses.co.uk>
2016-01-05 17:32:37 +0000

    Add webpack conf

---
+++ src/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require("path");
+module.exports = {
+  entry: {
+    app: ["./app/index.ts"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  },
+  module: {
+      loaders: [
+          { test: /\.tsx?/, loader: "webpack-typescript?target=ES5&jsx=react" },
+          { test: /\.html/, loader: "html" },
+      ]
+  },
+  resolve: {
+      extensions: ["", ".ts", ".js"]
+  }
+
+};
