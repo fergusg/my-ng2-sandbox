429463c82157b60852235981648ae24904ad505f
Fergus Gallagher <fergus@gooses.co.uk>
2016-01-07 13:07:13 +0000

    Fire change detection every 5s

--- src/app/components/immutable/immutable.ts
+++ src/app/components/immutable/immutable.ts
@@ -1,4 +1,4 @@
-import {Component, ChangeDetectionStrategy, ChangeDetectorRef} from "angular2/core";
+import {Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy} from "angular2/core";
 declare var Immutable: any;
 
 @Component({
@@ -8,26 +8,26 @@ declare var Immutable: any;
         <div>{{data | json}}</div>
         <button (click)="true">Click</button>
         <p>The value is incremented every 100ms but automatic change detection is disabled.</p>
-        <p>Click the button to trigger a manual check</p>
+        <p>Click the button to trigger a manual check.  One will be triggered every 5s anyway</p>
     `
 })
-class ImmutableComponent {
+class ImmutableComponent implements OnDestroy {
     private data: any;
     private interval: any;
 
     constructor(private ref: ChangeDetectorRef) {
         this.data = Immutable.Map({ x: 0 });
-        this.interval = setInterval(this.incr.bind(this, false), 100);
+        this.interval = setInterval(this.incr.bind(this), 100);
     }
 
     /**
      * Timeout/interval events to not trigger change detection with OnPush
      * Click events do, though.
      */
-    public incr(mark: boolean = false): void {
-        const x = this.data.get("x");
-        this.data = this.data.set("x", x + 1);
-        if (mark) {
+    public incr(): void {
+        const x = this.data.get("x") + 1;
+        this.data = this.data.set("x", x);
+        if (x % 50 === 0) {
             this.ref.markForCheck();
         }
     }
